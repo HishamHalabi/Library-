7d4e7be46fa276284968a3cd856cde613b254e1f
HishamHalabi <159281419+HishamHalabi@users.noreply.github.com>
2025-01-01 13:05:39 +0200

    Update User.h

--- User.h
+++ User.h
@@ -13,8 +13,12 @@ public:
     std::unordered_map<int, Book*> borrowedBooks;
     std::list<Transaction> transactionHistory;
 
-    User(int id, std::string name);
-    void addTransaction(std::string action, Book* book);
+    User(int id, std::string name) : id(id), name(name) {}
+
+    void addTransaction(std::string action, Book* book) {
+        Transaction transaction(action, book);
+        transactionHistory.push_back(transaction);
+    }
 };
 
 #endif
